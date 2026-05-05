00bc8964b03488882b86bf24605b3a1ef0aff7a6
ccaf0942 <ccaf0942@gmail.com>
2026-05-05 22:28:28 +0300

    dsadsa

--- chels.py
+++ chels.py
@@ -1,11 +1,1 @@
-import requests
-import datetime
-
-print(f"Запуск: {datetime.datetime.now()}")
-try:
-    r = requests.get("https://httpbin.org/get", timeout=5)
-    print(f"Статус: {r.status_code}")
-    data = r.json()
-    print(f"Host: {data['headers']['Host']}")
-except Exception as e:
-    print(f"Ошибка: {e}")
+print("hello wotrdl")
