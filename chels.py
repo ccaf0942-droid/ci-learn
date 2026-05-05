2887de40794b06b3a73e9bb8d8f937b0ac48ffee
ccaf0942 <ccaf0942@gmail.com>
2026-05-05 19:03:42 +0300

    EXALA

--- chels.py
+++ chels.py
@@ -1,12 +1,11 @@
 import requests
+import datetime
 
+print(f"Запуск: {datetime.datetime.now()}")
 try:
-    r = requests.get("https://api.github.com", timeout=5)
+    r = requests.get("https://httpbin.org/get", timeout=5)
     print(f"Статус: {r.status_code}")
-    print("Зависимость requests работает!")
-    print("DOWN?")
+    data = r.json()
+    print(f"Host: {data['headers']['Host']}")
 except Exception as e:
     print(f"Ошибка: {e}")
-    print("?NODOWN?")
-
-print("GELLO WOTROL>FDOP!!!!!!!!")
