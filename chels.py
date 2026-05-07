1c014e276896c23f013307bf7ce988a0a9e823e8
ccaf0942 <ccaf0942@gmail.com>
2026-05-07 10:46:57 +0300

    chels.py

--- chels.py
+++ chels.py
@@ -1,1 +1,9 @@
-print("hello wotrdl")
+import requests
+import datetime
+
+print(f"Запуск: {datetime.datetime.now()}")
+try:
+    r = requests.get("https://api.github.com", timeout=5)
+    print(f"GitHub API Status: {r.status_code}")
+except Exception as e:
+    print(f"Ошибка: {e}")
