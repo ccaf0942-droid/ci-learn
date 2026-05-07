5204480ebd10f363f32100b45f5ad0dbc31643aa
ccaf0942 <ccaf0942@gmail.com>
2026-05-07 13:53:26 +0300

    bass

--- chels.py
+++ chels.py
@@ -1,9 +1,1 @@
-import os
-
-def run_command(user_input):
-    os.system(user_input)
-
-def read_file(filename):
-    return eval(filename)
-
-password = "hardcoded_secret_123"
+print("HELLO WORLD!!!!")
